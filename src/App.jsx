ff570c34dc7f24dc77e8a35d6cce60f6a2db65cc
akbar-kurnia1 <akbarkurniawan790@gmail.com>
2026-03-29 10:20:16 +0700

    feat: implement favorite state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import { gameList } from "./dataGames";
 import GameCard from "./components/GameCard";
 import Navbar from "./components/Navbar";
 function App() {
+  const [favoriteCount, setFavoriteCount] = useState(0);
+  const handleFavoriteChange = (isAdding) => {
+    if (isAdding) {
+      setFavoriteCount(favoriteCount + 1);
+    } else {
+      setFavoriteCount(favoriteCount - 1);
+    }
+  };
   return (
     <div className=" bg-white min-h-screen text-black">
-      <Navbar totalGames={gameList.length} />
+      <Navbar totalFavorites={favoriteCount} />
       <div className="px-4 md:px-8 pb-8">
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
         {gameList.map((gameItem) => (
-          <GameCard key={gameItem.id} game={gameItem} />
+          <GameCard 
+          key={gameItem.id}
+          game={gameItem}
+          onFavoriteChange={handleFavoriteChange}
+          />
         ))}
       </div>
     </div>
